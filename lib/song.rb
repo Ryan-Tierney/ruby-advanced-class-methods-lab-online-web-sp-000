060c6008f59decb2901efc135b1b2701c9695b96
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-18 07:02:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,9 @@ class Song
     self.class.all << self
   end
 
-  def self.create 
-    song = self.new 
-    song 
-  end 
+ def self.create
+    song = self.new
+    #@@all << song
+    song
+  end
 end
