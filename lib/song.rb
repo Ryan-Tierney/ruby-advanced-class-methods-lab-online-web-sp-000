0c62930a9705a3a5d3ac3f6cdbd8124d9f320bdd
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-18 07:18:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,13 @@ end
 end
 
   def self.new_from_filename(name)
-  end 
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
 
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
 end 
